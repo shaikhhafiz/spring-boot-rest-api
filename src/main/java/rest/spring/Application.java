e03a537da349dd936a4bc93c4339247b9972aab8
Hafiz Ahmed <rhafizubd@gmail.com>
2018-04-22 22:05:48 +0600

    Added context path

--- src/main/java/rest/spring/Application.java
+++ src/main/java/rest/spring/Application.java
@@ -2,18 +2,13 @@ package rest.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application extends SpringBootServletInitializer {
+public class Application {
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
